c3111edf70aea2b908e9a7a9f3fb7a9d62ebda81
BaeDongWoo <ehddn5410@naver.com>
2023-01-13 22:48:05 +0900

    comments props 리스트를 만들어 map을 이용

--- React_Learn/my-app/src/chapter_05/CommentList.jsx
+++ React_Learn/my-app/src/chapter_05/CommentList.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
 import Comment from './Comment';
-
+const comments = [
+  {
+    name: '배동우',
+    comment: '안녕하세요, 우리 소통해요.',
+  },
+  {
+    name: '안세훈',
+    comment: '저는 상섬입니다.',
+  },
+  {
+    name: '한경빈',
+    comment: '저는 까까오입니다.',
+  },
+];
 function CommentList(props) {
   return (
     <div>
-      <Comment name={'배동우'} comment={'안녕하세요, 우리 소통해요.'} />
-      <Comment name={'안세훈'} comment={'저는 삼성입니다.'} />
-      <Comment name={'한경빈'} comment={'저는 까까온데용.'} />
+      {comments.map((comment) => {
+        return <Comment name={comment.name} comment={comment.comment} />;
+      })}
     </div>
   );
 }
